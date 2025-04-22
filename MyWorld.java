2f579120f3c55db774e65eb9a80c1e5e2aa6b570
LeooSo <sol542939@gmail.com>
2025-04-22 14:19:48 -0400

    Add banana

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Banana banana = new Banana();
+        addObject(banana, 600, 100);
     }
 }
