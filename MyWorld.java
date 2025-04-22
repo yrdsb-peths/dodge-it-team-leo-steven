7f05806547dad6c6a6d94defcec886756f3277ac
Steven542430 <440037784@gapps.yrdsb.ca>
2025-04-22 14:18:15 -0400

    Added pig movement

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Pig pig = new Pig();
+        addObject(pig, 100, 100);
     }
 }
